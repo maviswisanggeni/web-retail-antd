98915d457eb6b6375482acb706f43ac80c95eeff
Haykal Hanif <mhaykalhanif.29@gmail.com>
2023-10-17 13:47:37 +0700

    form pembelian and penjualan

--- src/pages/pembelian/FormPembelian.tsx
+++ src/pages/pembelian/FormPembelian.tsx
@@ -1,12 +1,92 @@
-import Title from "antd/es/typography/Title";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Typography, Form, Input, Button, Table, Space, Popconfirm } from "antd";
+import { PlusOutlined, DeleteOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
+
+interface ProductsEntity {
+  productName: string;
+  quantity: number;
+}
 
 const FormPembelian: React.FC = () => {
-    return (
-        <div style={{ textAlign: "center" }}>
-            <Title>Form Pembelian</Title>
-        </div>
-    );
+  const [form] = Form.useForm();
+  const [purchases, setPurchases] = useState<ProductsEntity[]>([]);
+  const [newPurchase, setNewPurchase] = useState<ProductsEntity>({
+    productName: "",
+    quantity: 0,
+  });
+
+  useEffect(() => {
+    // Load purchases from your data source or API
+  }, []);
+
+  const handleAddPurchase = () => {
+    setPurchases([...purchases, newPurchase]);
+    setNewPurchase({ productName: "", quantity: 0 });
+    form.resetFields();
+  };
+
+  const handleDeletePurchase = (record: ProductsEntity) => {
+    const updatedPurchases = purchases.filter((item) => item !== record);
+    setPurchases(updatedPurchases);
+  };
+
+  const columns = [
+    {
+      title: "Nama Produk",
+      dataIndex: "productName",
+      key: "productName",
+    },
+    {
+      title: "Jumlah",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      render: (text: any, record: ProductsEntity) => (
+        <Space size="small">
+          <Popconfirm
+            title="Are you sure to delete this purchase?"
+            onConfirm={() => handleDeletePurchase(record)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button type="link" icon={<DeleteOutlined />} />
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div className="content">
+      <Title>Pembelian</Title>
+      <Form form={form} layout="inline">
+        <Form.Item label="Nama Produk" name="productName">
+          <Input
+            value={newPurchase.productName}
+            onChange={(e) => setNewPurchase({ ...newPurchase, productName: e.target.value })}
+          />
+        </Form.Item>
+        <Form.Item label="Jumlah" name="quantity">
+          <Input
+            type="number"
+            value={newPurchase.quantity}
+            onChange={(e) => setNewPurchase({ ...newPurchase, quantity: parseInt(e.target.value) })}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" icon={<PlusOutlined />} onClick={handleAddPurchase}>
+            Tambah Pembelian
+          </Button>
+        </Form.Item>
+      </Form>
+      <Table columns={columns} dataSource={purchases} />
+    </div>
+  );
 };
 
 export default FormPembelian;
--- src/pages/penjualan/FormPenjualan.tsx
+++ src/pages/penjualan/FormPenjualan.tsx
@@ -1,13 +1,94 @@
-import Title from "antd/es/typography/Title";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Typography, Form, Input, Button, Table, Space, Popconfirm } from "antd";
+import { PlusOutlined, DeleteOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
+
+// Define the ProductsEntity type here
+interface ProductsEntity {
+  productName: string;
+  quantity: number;
+  // Add other properties if needed
+}
 
 const FormPenjualan: React.FC = () => {
-    return (
-        <div style={{ textAlign: "center" }}>
-            <Title>Form Penjualan</Title>
+  const [form] = Form.useForm();
+  const [purchases, setPurchases] = useState<ProductsEntity[]>([]); // Specify the type
+  const [newPurchase, setNewPurchase] = useState<ProductsEntity>({
+    productName: "",
+    quantity: 0,
+  });
 
-        </div>
-    );
+  useEffect(() => {
+    // Load purchases from your data source or API
+  }, []);
+
+  const handleAddPurchase = () => {
+    setPurchases([...purchases, newPurchase]);
+    setNewPurchase({ productName: "", quantity: 0 });
+    form.resetFields();
+  };
+
+  const handleDeletePurchase = (record: ProductsEntity) => {
+    const updatedPurchases = purchases.filter((item) => item !== record);
+    setPurchases(updatedPurchases);
+  };
+
+  const columns = [
+    {
+      title: "Nama Produk",
+      dataIndex: "productName",
+      key: "productName",
+    },
+    {
+      title: "Jumlah",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      render: (text: any, record: ProductsEntity) => (
+        <Space size="small">
+          <Popconfirm
+            title="Are you sure to delete this purchase?"
+            onConfirm={() => handleDeletePurchase(record)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button type="link" icon={<DeleteOutlined />} />
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div className="content">
+      <Title>Penjualan</Title>
+      <Form form={form} layout="inline">
+        <Form.Item label="Nama Produk" name="productName">
+          <Input
+            value={newPurchase.productName}
+            onChange={(e) => setNewPurchase({ ...newPurchase, productName: e.target.value })}
+          />
+        </Form.Item>
+        <Form.Item label="Jumlah" name="quantity">
+          <Input
+            type="number"
+            value={newPurchase.quantity}
+            onChange={(e) => setNewPurchase({ ...newPurchase, quantity: parseInt(e.target.value) })}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" icon={<PlusOutlined />} onClick={handleAddPurchase}>
+            Tambah Penjualan
+          </Button>
+        </Form.Item>
+      </Form>
+      <Table columns={columns} dataSource={purchases} />
+    </div>
+  );
 };
 
 export default FormPenjualan;
