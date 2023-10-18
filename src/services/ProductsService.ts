7d959bcb079cd6c874a2b5d001935797e7ce15a5
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2023-10-18 15:00:45 +0700

    add: [MW] Delete and Edit Product

--- src/services/ProductsService.ts
+++ src/services/ProductsService.ts
@@ -7,4 +7,19 @@ export class ProductsService {
         const ProductURL: string = `${this.URL}/products`
         return axios.get(ProductURL);
     }
+
+    public static deleteProduct(id: number) {
+        const ProductURL: string = `${this.URL}/products/${id}`
+        return axios.delete(ProductURL);
+    }
+
+    public static updateProduct(id: number, data: unknown) {
+        const ProductURL: string = `${this.URL}/products/${id}`
+        return axios.put(ProductURL, data);
+    }
+
+    public static getProduct(id: number) {
+        const ProductURL: string = `${this.URL}/products/${id}`
+        return axios.get(ProductURL);
+    }
 }
