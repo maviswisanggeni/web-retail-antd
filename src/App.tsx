a527ea20154a1434d51e43639ade0b75980c9224
Haykal Hanif <mhaykalhanif.29@gmail.com>
2023-10-23 00:11:13 +0700

    excel export and several new package

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FormPembelian from "./pages/pembelian/FormPembelian";
-import FormPenjualan from "./pages/penjualan/FormPenjualan";
+import React from 'react';
+import Dashboard from './pages/dashboard/Dashboard';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/pembelian" element={<FormPembelian />} />
-        <Route path="/penjualan" element={<FormPenjualan />} />
-      </Routes>
-    </Router>
+    <Dashboard /> 
   );
-}
+};
 
 export default App;
--- src/pages/pembelian/FormPembelian.tsx
+++ src/pages/pembelian/FormPembelian.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Typography, Form, Input, Button, Table, Space, Popconfirm, Modal, Select } from "antd";
 import { PlusOutlined, EditOutlined, DeleteOutlined, InfoCircleOutlined } from "@ant-design/icons";
 import axios from "axios";
+import XLSX from "xlsx";
 
 const { Title } = Typography;
 const { Option } = Select;
@@ -45,7 +46,6 @@ const FormPembelian: React.FC = () => {
   }, [purchases]);
 
   const handleAddPurchase = () => {
-
     const cost = newPurchase.quantity * 100;
     const purchaseWithCost = { ...newPurchase, cost };
 
@@ -71,6 +71,16 @@ const FormPembelian: React.FC = () => {
 
   const getTotalCost = () => {
     return purchases.reduce((total, purchase) => total + purchase.cost, 0);
+  };
+
+  // Function to export data to Excel
+  const exportToExcel = () => {
+    const worksheet = XLSX.utils.json_to_sheet(purchases);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Purchases");
+    
+    // Generate an Excel file and trigger a download
+    XLSX.writeFile(workbook, "purchases.xlsx");
   };
 
   const columns = [
@@ -134,43 +144,14 @@ const FormPembelian: React.FC = () => {
   return (
     <div className="content">
       <Title>Transaksi Pembelian</Title>
+
+      {/* Add the export button here */}
+      <Button type="primary" onClick={exportToExcel}>
+        Export to Excel
+      </Button>
+
       <Form form={form} layout="inline">
-        <Form.Item label="Sales" name="sales">
-          <Select
-            value={newPurchase.sales}
-            onChange={(value) => setNewPurchase({ ...newPurchase, sales: value })}
-          >
-            {salesData.map((sale, index) => (
-              <Option key={index} value={sale}>
-                {sale}
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Form.Item label="Nama Produk" name="productName">
-          <Input
-            value={newPurchase.productName}
-            onChange={(e) => setNewPurchase({ ...newPurchase, productName: e.target.value })}
-          />
-        </Form.Item>
-        <Form.Item label="Jumlah" name="quantity">
-          <Input
-            type="number"
-            value={newPurchase.quantity}
-            onChange={(e) => setNewPurchase({ ...newPurchase, quantity: parseInt(e.target.value) })}
-          />
-        </Form.Item>
-        <Form.Item label="Customer" name="customer">
-          <Input
-            value={newPurchase.customer}
-            onChange={(e) => setNewPurchase({ ...newPurchase, customer: e.target.value })}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" icon={<PlusOutlined />} onClick={handleAddPurchase}>
-            Tambah Pembelian
-          </Button>
-        </Form.Item>
+        {/* ... (rest of your form code) */}
       </Form>
       <Table columns={columns} dataSource={purchases} />
       <div>
@@ -182,12 +163,7 @@ const FormPembelian: React.FC = () => {
         onOk={() => setIsDetailModalVisible(false)}
         onCancel={() => setIsDetailModalVisible(false)}
       >
-        {selectedRecord && (
-          <div>
-            <p>Sales: {selectedRecord.sales}</p>
-            <p>Customer: {selectedRecord.customer}</p>
-          </div>
-        )}
+        {/* ... (modal content) */}
       </Modal>
       <Modal
         title="Edit Pembelian"
@@ -195,7 +171,7 @@ const FormPembelian: React.FC = () => {
         onOk={() => setIsEditModalVisible(false)}
         onCancel={() => setIsEditModalVisible(false)}
       >
-        {/* Edit form for the selected record */}
+        {/* ... (edit modal content) */}
       </Modal>
     </div>
   );
