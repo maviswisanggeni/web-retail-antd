a4eb9f0d0f8ee9db14ee931c8b8753fe9e8f0578
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2023-10-27 14:48:58 +0700

    fetch: [MW] API CRUD Product from Back-End

---
+++ src/models/IAdjustment.ts
@@ -0,0 +1,8 @@
+export interface IAdjustment {
+  id: number;
+  product_id: number;
+  product_quantity: number;
+  opname_info: string;
+  createdAt: string;
+  updatedAt: string;
+}
--- src/services/ProductsService.ts
+++ src/services/ProductsService.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios"
 
 export class ProductsService {
-    private static URL: string = 'https://dummyjson.com'
-    private static URL2: string = 'http://localhost:3000'
+    private static URL: string = 'http://localhost:3000'
 
     // config cors 
     private static config: AxiosRequestConfig = {
@@ -13,27 +12,27 @@ export class ProductsService {
     }
 
     public static getAllProducts() {
-        const ProductURL: string = `${this.URL2}/product`
+        const ProductURL: string = `${this.URL}/product`
         return axios.get(ProductURL);
     }
 
     public static createProduct(data: unknown) {
-        const ProductURL: string = `${this.URL2}/product`
+        const ProductURL: string = `${this.URL}/product`
         return axios.post(ProductURL, data, this.config);
     }
 
     public static deleteProduct(id: number) {
-        const ProductURL: string = `${this.URL2}/product/${id}`
+        const ProductURL: string = `${this.URL}/product/${id}`
         return axios.delete(ProductURL, this.config);
     }
 
     public static updateProduct(id: number, data: unknown) {
-        const ProductURL: string = `${this.URL2}/product/${id}`
+        const ProductURL: string = `${this.URL}/product/${id}`
         return axios.patch(ProductURL, data, this.config);
     }
 
     public static getProductById(id: number) {
-        const ProductURL: string = `${this.URL2}/product/${id}`
+        const ProductURL: string = `${this.URL}/product/${id}`
         return axios.get(ProductURL, this.config);
     }
 }
